609a19197b4c5baba2272f0bb46f4d2f444d24a0
arunas.kareckas <arunas.kareckas@zenitech.co.uk>
2021-01-30 12:02:50 +0200

    updated mutational tests

--- core/src/main/java/uk/co/zenitech/factory/ArtistFactory.java
+++ core/src/main/java/uk/co/zenitech/factory/ArtistFactory.java
@@ -1,6 +1,10 @@
 package uk.co.zenitech.factory;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.apache.commons.lang3.RandomUtils;
@@ -9,26 +13,38 @@ import uk.co.zenitech.domain.Artist;
 import uk.co.zenitech.domain.Genre;
 
 public class ArtistFactory {
-    private static final int MAX_ALBUMS_EXCLUSIVE = 4;
-    private static final int MAX_SONGS_EXCLUSIVE = 11;
-    private static final int MAX_DAYS_IN_SEPT_EXCLUSIVE = 31;
 
     public Artist delegateCreateArtist(String name, Genre genre) {
         return createArtist(name, genre);
     }
 
+    private static final int MAX_DAYS_IN_MONTH_EXCLUSIVE = 30;
+    private static final int MAX_SONGS_EXCLUSIVE = 11;
+
     public static Artist createArtist(String name, Genre genre) {
-        var songs = IntStream.rangeClosed(1, RandomUtils.nextInt(1, MAX_SONGS_EXCLUSIVE))
-            .mapToObj(i  -> String.format("song_%d", i))
-            .collect(Collectors.toList());
+        return createArtist(name, genre, Month.SEPTEMBER, 1);
+    }
 
-        var albums = IntStream.rangeClosed(1, RandomUtils.nextInt(1, MAX_ALBUMS_EXCLUSIVE))
-            .mapToObj(i -> new Album(
-                String.format("album_%d", i),
-                LocalDate.of(2020, 9, RandomUtils.nextInt(1, MAX_DAYS_IN_SEPT_EXCLUSIVE)),
-                songs))
-            .collect(Collectors.toSet());
+    public static Artist createArtist(String name, Genre genre, Month month, int albums) {
+        return new Artist(name, genre, createAlbums(month, albums));
+    }
 
-        return new Artist(name, genre, albums);
+    public static Set<Album> createAlbums(Month month, int albums) {
+        if (albums > 0) {
+            return IntStream.rangeClosed(1, RandomUtils.nextInt(1, albums)).mapToObj(
+                    i -> new Album(String.format("album_%d", i),
+                            LocalDate.of(2020, month.getValue(),
+                                    RandomUtils.nextInt(1, MAX_DAYS_IN_MONTH_EXCLUSIVE)),
+                            createSongs()))
+                    .collect(Collectors.toSet());
+        } else {
+            return Collections.emptySet();
+        }
+    }
+
+    public static List<String> createSongs() {
+        return IntStream.rangeClosed(1, RandomUtils.nextInt(1, MAX_SONGS_EXCLUSIVE))
+                .mapToObj(i  -> String.format("song_%d", i))
+                .collect(Collectors.toList());
     }
 }
